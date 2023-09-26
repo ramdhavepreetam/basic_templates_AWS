5a927760eb812905ebbd83729dd19f467221062e
Preetam Ramdhave <ramdhavepreetam@gmail.com>
2023-09-26 15:55:21 -0700

    Update lambda.netforuploadse.cs

--- lambda.netforuploadse.cs
+++ lambda.netforuploadse.cs
@@ -1,116 +1,91 @@
-Here is one way to convert the Python code to C# for .NET Core:
-
-```csharp
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Threading.Tasks;
+using Amazon.Lambda.Core;
 using Amazon.SecretsManager;
 using Amazon.SecretsManager.Model;
 using Amazon.S3;
-using Amazon.S3.Model;
 using Npgsql;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Data;
+using System.IO;
+using System.Text;
 
-public class Function 
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+public class Function
 {
-  private static IAmazonSecretsManager secretsClient;
-  private static IAmazonS3 s3Client;
-  
-  public async Task<string> FunctionHandler(SNSEvent evnt, ILambdaContext context)
-  {
-    try 
-    {
-      var secrets = await GetSecrets("secretID");
-      
-      var dbParams = new NpgsqlConnectionStringBuilder{
-        Host = secrets["host"],
-        Port = int.Parse(secrets["port"]),
-        Username = secrets["user"],
-        Password = secrets["password"],
-        Database = secrets["dbname"]
-      };
+    private const string SecretId = "ppd-dev-pricefile-modernization";
 
-      using(var conn = new NpgsqlConnection(dbParams.ConnectionString)) {
-        conn.Open();
-      
-        var initialData = await FetchData(conn, "SELECT DISTINCT dealer_code FROM dealer_master");
-        
-        foreach(var row in initialData) {
-          var dealerCode = row[0];
-          
-          var query = $"SELECT * FROM dcpp0113c dd WHERE dd.itmid IN (SELECT dc.itmid FROM dcpp0115 dc WHERE dc.cstno = '{dealerCode}')";
-          
-          var data = await FetchData(conn, query);
-          
-          await CreateS3File(data, dealerCode, secrets["bucketName"]);
+    public JObject GetSecrets()
+    {
+        using var client = new AmazonSecretsManagerClient();
+        var response = client.GetSecretValue(new GetSecretValueRequest { SecretId = SecretId });
+        return JObject.Parse(response.SecretString);
+    }
+
+    public string GetConnectionString(JObject secrets)
+    {
+        return $"Host={secrets["host"]};Username={secrets["user"]};Password={secrets["password"]};Database={secrets["dbname"]};Port={secrets["port"]}";
+    }
+
+    public void CreateS3Folder(DataTable allData, string dealerID, string bucketName)
+    {
+        var output = new StringBuilder();
+        foreach (DataRow row in allData.Rows)
+        {
+            for (var i = 0; i < allData.Columns.Count; i++)
+            {
+                output.Append(row[i].ToString() + ",");
+            }
+            output.Length--; // Remove the last comma
+            output.AppendLine();
         }
-      }
 
-      return "Done!";
-      
-    } catch(Exception ex) {
-      return ex.Message; 
+        using var client = new AmazonS3Client();
+        var folderName = $"{dealerID}_{DateTime.Now:yyyy_MM_dd}";
+        var s3Key = $"{folderName}/ITEM.csv";
+
+        var putRequest = new Amazon.S3.Model.PutObjectRequest
+        {
+            BucketName = bucketName,
+            Key = s3Key,
+            ContentBody = output.ToString()
+        };
+        client.PutObject(putRequest);
     }
-  }
 
-  private async Task<Dictionary<string, string>> GetSecrets(string secretId)
-  {
-    var response = await secretsClient.GetSecretValueAsync(new GetSecretValueRequest 
+    public object LambdaHandler(ILambdaContext context)
     {
-      SecretId = secretId
-    });
+        var secrets = GetSecrets();
+        var connectionString = GetConnectionString(secrets);
 
-    return JsonConvert.DeserializeObject<Dictionary<string, string>>(response.SecretString);
-  }
+        using var conn = new NpgsqlConnection(connectionString);
+        conn.Open();
+        
+        var cmd = new NpgsqlCommand("SELECT distinct \"DEALER_CODE\" FROM ppdglobal.\"DEALER_MASTER\"", conn);
+        var adapter = new NpgsqlDataAdapter(cmd);
+        
+        var initialData = new DataTable();
+        adapter.Fill(initialData);
 
-  private async Task<List<string[]>> FetchData(NpgsqlConnection conn, string query)
-  {
-    using(var cmd = new NpgsqlCommand(query, conn))
-    using(var reader = await cmd.ExecuteReaderAsync()) {
-      var results = new List<string[]>();
-      while(await reader.ReadAsync()) {
-        var values = new string[reader.FieldCount];
-        reader.GetValues(values);
-        results.Add(values);
-      }
-      return results;
+        foreach (DataRow row in initialData.Rows)
+        {
+            var dealerCode = row[0].ToString().Replace(" ", string.Empty);
+            var secondaryQuery = $"SELECT * FROM PPDGLOBAL.\"DCPP0113C\" DD WHERE DD.\"ITMID\" IN ((SELECT DC.\"ITMID\" FROM PPDGLOBAL.\"DCPP0115\" DC WHERE  DC.\"CSTNO\" = '{dealerCode}'))";
+            var secondaryCmd = new NpgsqlCommand(secondaryQuery, conn);
+            var secondaryAdapter = new NpgsqlDataAdapter(secondaryCmd);
+            
+            var secondaryData = new DataTable();
+            secondaryAdapter.Fill(secondaryData);
+
+            CreateS3Folder(secondaryData, dealerCode, secrets["BucketName"].ToString());
+        }
+
+        conn.Close();
+
+        return new 
+        {
+            statusCode = 200,
+            body = "Data successfully uploaded to S3!"
+        };
     }
-  }
-
-//...
-
-private async Task CreateS3File(List<string[]> data, string dealerId, string bucketName)  
-{
-  var folderName = $"{dealerId}_{DateTime.UtcNow:yyyy_MM_dd}";
-  var key = $"{folderName}/data.csv";
-
-  using (var writer = new StringWriter())
-  {
-    using (var csv = new CsvWriter(writer, System.Globalization.CultureInfo.InvariantCulture))
-    {
-      foreach (var row in data)
-      {
-        csv.WriteField(row);
-        csv.NextRecord();
-      }
-    }
-
-    var contents = writer.ToString();
-
-    // upload to S3
-  }
 }
-
-}
-```
-
-The key differences:
-
-- Use C# async/await for async operations instead of Python's async framework
-- Convert Python libraries to .NET equivalents (Npgsql instead of psycopg2, AWSSDK instead of boto3, System.IO instead of io, etc.)
-- Dictionary instead of Python dict for secrets 
-- String interpolation with $ for queries instead of f-strings
-- Use C# CSV helper instead of Python CSV module
-- Handle exceptions differently since no try/except blocks in C# async methods
-
-Let me know if any part needs more explanation!
