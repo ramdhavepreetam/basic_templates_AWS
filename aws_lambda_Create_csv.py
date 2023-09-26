77dc46fca1b381caebd8fa72d282f31deec139f9
Preetam Ramdhave <ramdhavepreetam@gmail.com>
2023-09-26 10:07:45 -0700

    Update aws_lambda_Create_csv.py
    Code changes donef or the program

--- aws_lambda_Create_csv.py
+++ aws_lambda_Create_csv.py
@@ -5,61 +5,88 @@ from io import StringIO
 from datetime import datetime
 import json
 import logging
-# Fetch the secret from AWS Secrets Manager
-secrets_client = boto3.client('secretsmanager')
-response = secrets_client.get_secret_value(
-    SecretId='ppd-dev-pricefile-modernization')
-secrets = json.loads(response['SecretString'])
 
-DB_PARAMS = {
-    'dbname': secrets['dbname'],
-    'user': secrets['user'],
-    'password': secrets['password'],
-    'host': secrets['host'],
-    'port': secrets['port']
-}
+logging.basicConfig(level=logging.INFO)
 
-# Create a CSV in-memory
+def get_secrets(secret_id):
+    try:
+        secrets_client = boto3.client('secretsmanager')
+        response = secrets_client.get_secret_value(SecretId=secret_id)
+        secrets = json.loads(response['SecretString'])
+        return secrets
+    except Exception as e:
+        logging.error(f"Error fetching secrets: {e}")
+        raise
 
+def connect_to_db(db_params):
+    try:
+        conn = psycopg2.connect(**db_params)
+        return conn
+    except Exception as e:
+        logging.error(f"Error connecting to the database: {e}")
+        raise
 
-def create_s3_folder(all_data, dealerID):
+def fetch_data_from_db(cur, query):
+    try:
+        cur.execute(query)
+        return cur.fetchall()
+    except Exception as e:
+        logging.error(f"Error executing query: {e}")
+        raise
 
-    output = StringIO()
-    csv_writer = csv.writer(output)
-    csv_writer.writerows(all_data)
-    # Upload the CSV to Amazon S3
-    s3_client = boto3.client('s3')
-    s3_bucket = secrets['BucketName']
-    folder_name = "{}_{}".format(dealerID, datetime.now().strftime('%Y_%m_%d'))
-    s3_key = f"{folder_name}/ITEM.csv"
-    se_key_utfencoded = s3_key.encode('utf-8')
-    output.seek(0)
-    logging.info(output.getvalue())
-    # s3_client.upload_fileobj(output, s3_bucket, s3_key)
-    resource = boto3.resource('s3')
-    resource.Object(s3_bucket, s3_key).put(Body=output.getvalue())
+def create_s3_folder(all_data, dealerID, bucket_name):
+    try:
+        output = StringIO()
+        csv_writer = csv.writer(output)
+        csv_writer.writerows(all_data)
+        
+        folder_name = "{}_{}".format(dealerID, datetime.now().strftime('%Y_%m_%d'))
+        s3_key = f"{folder_name}/ITEM.csv"
+        
+        output.seek(0)
+        
+        resource = boto3.resource('s3')
+        resource.Object(bucket_name, s3_key).put(Body=output.getvalue())
+        logging.info(f"Successfully uploaded to {s3_key}")
+    except Exception as e:
+        logging.error(f"Error creating S3 folder: {e}")
+        raise
 
+def lambda_handler(event, context):
+    try:
+        secrets = get_secrets('ppd-dev-pricefile-modernization')
+        
+        DB_PARAMS = {
+            'dbname': secrets['dbname'],
+            'user': secrets['user'],
+            'password': secrets['password'],
+            'host': secrets['host'],
+            'port': secrets['port']
+        }
+        
+        conn = connect_to_db(DB_PARAMS)
+        cur = conn.cursor()
+        
+        initial_data = fetch_data_from_db(cur, "SELECT distinct \"DEALER_CODE\" FROM ppdglobal.\"DEALER_MASTER\"")
+        
+        for item in initial_data:
+            query = f"SELECT * FROM PPDGLOBAL.\"DCPP0113C\" DD WHERE DD.\"ITMID\" IN ((SELECT DC.\"ITMID\" FROM PPDGLOBAL.\"DCPP0115\" DC WHERE  DC.\"CSTNO\" = '{item[0].replace(' ', '')}'))"
+            secondary_data = fetch_data_from_db(cur, query)
+            
+            folder_value = item[0].replace(' ', '')
+            create_s3_folder(secondary_data, folder_value, secrets['BucketName'])
+        
+        cur.close()
+        conn.close()
+        
+        return {
+            'statusCode': 200,
+            'body': json.dumps('Data successfully processed and uploaded to S3!')
+        }
 
-# Connect to PostgreSQL
-conn = psycopg2.connect(**DB_PARAMS)
-cur = conn.cursor()
-
-# Execute initial SQL query
-initial_query = "SELECT distinct \"DEALER_CODE\" FROM ppdglobal.\"DEALER_MASTER\""
-cur.execute(initial_query)
-initial_data = cur.fetchall()
-
-# Loop through the first column and execute secondary queries
-all_data = []
-for item in initial_data:
-    secondary_query = f"SELECT * FROM PPDGLOBAL.\"DCPP0113C\" DD WHERE DD.\"ITMID\" IN ((SELECT DC.\"ITMID\" FROM PPDGLOBAL.\"DCPP0115\" DC WHERE  DC.\"CSTNO\" = '{item[0].replace(' ', '')}'))"
-    cur.execute(secondary_query)
-    secondary_data = cur.fetchall()
-    all_data.extend(secondary_data)
-    folder_value = item[0].replace(' ', '')
-    create_s3_folder(secondary_data, folder_value)
-
-
-# Close database connection
-cur.close()
-conn.close()
+    except Exception as e:
+        logging.error(f"Error in the Lambda handler: {e}")
+        return {
+            'statusCode': 500,
+            'body': json.dumps('An error occurred. Check logs for details.')
+        }
