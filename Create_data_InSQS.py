e8999597e97fbd0ece6b6cda9772adb95d7c3a8b
Preetam Ramdhave <ramdhavepreetam@gmail.com>
2023-09-28 14:00:36 -0700

    Update Create_data_InSQS.py

--- Create_data_InSQS.py
+++ Create_data_InSQS.py
@@ -87,3 +87,33 @@ def lambda_handler(event, context):
             'statusCode': 500,
             'body': json.dumps('An error occurred. Check logs for details.')
         }
+
+
+'''
+{
+  "Records": [
+    {
+      "messageId": "19dd0b57-b21e-4ac1-bd88-01bbb068cb78",
+      "receiptHandle": "MessageReceiptHandle",
+      "body":{
+        "dealer_code": "K035",
+        "filetype": "daily"
+      },
+      "attributes": {
+        "ApproximateReceiveCount": "1",
+        "SentTimestamp": "1523232000000",
+        "SenderId": "123456789012",
+        "ApproximateFirstReceiveTimestamp": "1523232000001"
+      },
+      "messageAttributes": {},
+      "md5OfBody": {
+        "dealer_code": "K035",
+        "filetype": "daily"
+      },
+      "eventSource": "aws:sqs",
+      "eventSourceARN": "arn:aws:sqs:us-east-1:123456789012:MyQueue",
+      "awsRegion": "us-east-1"
+    }
+  ]
+}
+'''
