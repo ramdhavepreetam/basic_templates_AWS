2a83391a0cfb59621e691571bf9f2c40873f988d
Preetam Ramdhave <ramdhavepreetam@gmail.com>
2023-09-28 14:02:48 -0700

    Update Create_data_InSQS.py

--- Create_data_InSQS.py
+++ Create_data_InSQS.py
@@ -95,10 +95,7 @@ def lambda_handler(event, context):
     {
       "messageId": "19dd0b57-b21e-4ac1-bd88-01bbb068cb78",
       "receiptHandle": "MessageReceiptHandle",
-      "body":{
-        "dealer_code": "K035",
-        "filetype": "daily"
-      },
+      "body": "{\"dealer_code\": \"K035\", \"filetype\": \"daily\"}",
       "attributes": {
         "ApproximateReceiveCount": "1",
         "SentTimestamp": "1523232000000",
@@ -106,14 +103,12 @@ def lambda_handler(event, context):
         "ApproximateFirstReceiveTimestamp": "1523232000001"
       },
       "messageAttributes": {},
-      "md5OfBody": {
-        "dealer_code": "K035",
-        "filetype": "daily"
-      },
+      "md5OfBody": "e4e68fb7bd0e697a0ae8f1bb342846b3",  // This is just a sample MD5 hash. In a real event, it would be computed based on the body's contents.
       "eventSource": "aws:sqs",
       "eventSourceARN": "arn:aws:sqs:us-east-1:123456789012:MyQueue",
       "awsRegion": "us-east-1"
     }
   ]
 }
+
 '''
